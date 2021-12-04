8319dae6457d94368a4a90a89824b3542cb3c892
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-12-04 16:41:49 +0100

    Vuelidate set up.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,14 +5,16 @@ import "viewerjs/dist/viewer.css";
 import VueViewer from "v-viewer";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-    faPlus,
-    faTrash,
-    faEdit,
-    faSearch,
+  faPlus,
+  faTrash,
+  faEdit,
+  faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import VCalendar from "v-calendar";
 import jQuery from "jquery";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 library.add(faTrash);
 library.add(faEdit);
@@ -24,10 +26,12 @@ window.$ = $;
 
 const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 app.use(VueViewer);
+
 app.use(VCalendar, {});
+app.use(VueAxios, axios);
 app.use(router).mount("#app");
 
 if (process.env.VUE_APP_ENV == "development") {
-    document.cookie = "userId=2; domain=localhost";
-    document.cookie = "email=testvillaowner@gmail.com; domain=localhost";
+  document.cookie = "userId=2; domain=localhost";
+  document.cookie = "email=testvillaowner@gmail.com; domain=localhost";
 }
--- frontend/src/views/InstructorServices.vue
+++ frontend/src/views/InstructorServices.vue
@@ -1,109 +1,98 @@
 <template>
-    <Header
-        headerText="Sunrise adventure"
-        subtitleText="Come and enjoy this adventure with us. Fishing in the sunrise is one of the most exciting experiences!"
-    />
-    <Navbar :baseUrl="baseUrlInstructor" :navbarItems="navbarItems" />
-    <div class="flexbox-container-space-between">
-        <h1>Services</h1>
-        <SearchBar searchPlaceholder="Search services..." />
-        <button>Add new service</button>
-    </div>
+  <Navbar :baseUrl="baseUrlInstructor" :navbarItems="navbarItems" />
+  <div class="flexbox-container-space-between">
+    <h1>Services</h1>
+    <SearchBar searchPlaceholder="Search services..." @filtered="onFiltered" />
+    <button @click="$router.push(baseUrlInstructor + 'new-adventure')">
+      Add new service
+    </button>
+  </div>
 
-    <div class="flexbox-container">
-        <InstructorServiceCard
-            v-for="service in services"
-            :key="service.id"
-            :service="service"
-        />
-    </div>
+  <div class="flexbox-container">
+    <InstructorServiceCard
+      v-for="service in services"
+      :key="service.id"
+      :service="service"
+    />
+  </div>
 </template>
 
 <script>
 import InstructorServiceCard from "../components/InstructorServiceCard.vue";
-import Header from "@/components/Header.vue";
 import Navbar from "@/components/Navbar.vue";
 import SearchBar from "@/components/SearchBar.vue";
+import axios from "../api/api.js";
 
 export default {
-    name: "InstructorServices",
-    components: {
-        InstructorServiceCard,
-        Header,
-        Navbar,
-        SearchBar,
+  name: "InstructorServices",
+  components: {
+    InstructorServiceCard,
+    Navbar,
+    SearchBar,
+  },
+  mounted() {
+    axios.get("/api/Adventure/GetAllOwnedAdventures").then((res) => {
+      this.services = res.data;
+      console.log(res.data);
+    });
+  },
+  data() {
+    return {
+      services: [],
+      navbarItems: [
+        "Services",
+        "Reservations",
+        "Availability",
+        "Analytics",
+        "My profile",
+      ],
+      baseUrlInstructor: "/instructor/",
+    };
+  },
+  methods: {
+    onFiltered(value) {
+      this.services = value;
     },
-    computed: {},
-    data() {
-        return {
-            services: [
-                {
-                    id: 1,
-                    name: "Sunrise adventure",
-                    description: "Promo description",
-                    address: "Bulevard of broken dreams 0",
-                },
-                {
-                    id: 2,
-                    name: "Sunset adventure",
-                    description: "Promo description",
-                    address: "Bulevard of broken dreams 1",
-                },
-                {
-                    id: 3,
-                    name: "Sunset adventure",
-                    description: "Promo description",
-                    address: "Bulevard of broken dreams 2",
-                },
-            ],
-            navbarItems: [
-                "Services",
-                "Reservations",
-                "Availability",
-                "Analytics",
-                "My profile",
-            ],
-            baseUrlInstructor: "/instructor/",
-        };
-    },
+  },
 };
 </script>
 
 <style scoped>
 .flexbox-container {
-    padding: 50px;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
+  padding: 50px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
 }
 
 .flexbox-container-space-between {
-    padding: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  padding: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 button {
-    background-color: #fff000;
-    border-radius: 12px;
-    color: #000;
-    cursor: pointer;
-    font-weight: bold;
-    padding: 10px 15px;
-    text-align: center;
-    transition: 200ms;
-    height: 50px;
-    box-sizing: border-box;
-    border: 0;
-    font-size: 16px;
-    user-select: none;
-    -webkit-user-select: none;
-    touch-action: manipulation;
-    margin-top: 7px;
+  background-color: #000;
+  border-radius: 12px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 10px 15px;
+  text-align: center;
+  transition: 200ms;
+  height: 50px;
+  box-sizing: border-box;
+  border: 0;
+  font-size: 16px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  margin-top: 7px;
 }
 
 button:hover {
-    background-color: #ccbf05;
+  background-color: #ccbf05;
 }
 </style>
